d91a308e72f97bce754c3cc3244ba06987b3a437
Minh Tien <thmt2504@gmail.com>
2025-06-11 16:42:43 +0700

    Upload full source

--- SET-370/liveDeliveryTracking.mjs
+++ SET-370/liveDeliveryTracking.mjs
@@ -1,36 +1,27 @@
-console.log("\n--- Bài tập 2: setInterval ---");
-
-let simulateCountdownMinutes = 5;
-
-const statusMessages = [
-  "Order confirmed, preparing your food (5 minutes left)...", // Index 0
-  "Driver is picking up your order (4 minutes left)...",      // Index 1
-  "Food is being prepared (3 minutes left)...",               // Index 2
-  "Driver is on the way (2 minutes left)...",                 // Index 3
-  "Driver is almost there (1 minute left)...",                // Index 4
-  "Order delivered!"                                          // Index 5
-];
-
 function updateDeliveryStatus() {
-  if (simulateCountdownMinutes >= 0) {
-      const messageIndex = Math.min(simulateCountdownMinutes, statusMessages.length - 1);
-      let currentMessage = statusMessages[statusMessages.length - 1 - messageIndex] || statusMessages[0];
-      if (simulateCountdownMinutes === 0) currentMessage = statusMessages[5];
+  let minutes = 5;
+  
+  const statusMessages = {
+      5: "Driver is picking up your order",
+      4: "Driver is on the way",
+      3: "Driver is approaching your location",
+      2: "Driver is nearby",
+      1: "Driver is just around the corner",
+      0: "Order delivered!"
+  };
 
-      console.log(`[${5 - simulateCountdownMinutes}s passed] ${currentMessage}`);
-      simulateCountdownMinutes--;
-  } else {
-      clearInterval(deliveryInterval);
-      console.log("Interval stopped.");
-  }
+  console.log(`[${minutes} min] ${statusMessages[minutes]}`);
+  
+  const intervalId = setInterval(() => {
+      minutes--;
+      
+      if (minutes === 0) {
+          console.log(`[${minutes} min] ${statusMessages[minutes]}`);
+          clearInterval(intervalId);
+      } else {
+          console.log(`[${minutes} min] ${statusMessages[minutes]}`);
+      }
+  }, 1000); // NOTE: Sử dụng 1000ms (1 giây) cho demo nhanh, theo đề bài thì dùng 60000ms (1 phút)
 }
 
 updateDeliveryStatus();
-// const deliveryInterval = setInterval(updateDeliveryStatus, 1000);
-// console.log(deliveryInterval);
-
-// setInterval(function, delay, ...args): trả về intervalId
-// giúp lặp lại callback function (là tham số function trong setInterval) sau mỗi khoảng thời gian delay (ms)
-// clearInterval(intervalId) để dừng lặp lại
-// code trên ví dụ lặp lại hàm updateDeliveryStatus sau mỗi 1s
-// tổng cộng có 7 lần lặp lại, lặp cho đến khi simulateCountdownMinutes = -1 (< 0) thì biểu thức else được kích hoạt, cho phép dừng interval
